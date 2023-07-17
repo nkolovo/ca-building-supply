9206190d7c3265f7ccdfe3372054d8aabcc7ca16
nkolovo <nikolas_kolovos@hotmail.com>
2023-07-17 15:14:55 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useEffect } from "react";
 
 import Layout from '../src/components/layout/Layout';
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+
     return (
         <div className="ca-building-supply">
             <title>CA Building Supply</title>
