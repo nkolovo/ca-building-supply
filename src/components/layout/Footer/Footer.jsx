0b24cbb1c2cf8d8315bbcb6cb684124b97481cc6
nkolovo <nikolas_kolovos@hotmail.com>
2023-09-06 18:05:41 -0400

    Mobile Changes
    Product images are larger
    
    Address in the footer doesn't cut off anymore

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -9,21 +9,21 @@ export default function Footer() {
                 </div>
                 <div className='footer_contact_us_info_gen'>
                     <span className='footer_contact_us_info_row'>Address:
-                        <a href="https://www.google.com/maps/place/60+Caster+Ave,+Woodbridge,+ON+L4L+5Y9/@43.78467,-79.5578496,17z/data=!3m1!4b1!4m6!3m5!1s0x882b2ff5ce7814e9:0xd8fd8cb8f531fd1!8m2!3d43.78467!4d-79.5578496!16s%2Fg%2F11c26kthj4?entry=ttu" style={{ color: "wheat" }}>
+                        <a href="https://www.google.com/maps/place/60+Caster+Ave,+Woodbridge,+ON+L4L+5Y9/@43.78467,-79.5578496,17z/data=!3m1!4b1!4m6!3m5!1s0x882b2ff5ce7814e9:0xd8fd8cb8f531fd1!8m2!3d43.78467!4d-79.5578496!16s%2Fg%2F11c26kthj4?entry=ttu" style={{ color: "wheat", alignSelf: "end" }}>
                             <span className="footer_contact_us_links">
                                 60 Caster Ave Woodbridge ON L4L 5Y9
                             </span>
                         </a>
                     </span>
                     <span className='footer_contact_us_info_row'>Phone Number: {' '}
-                        <a href="tel:+4168857330" style={{ color: "wheat" }}>
+                        <a href="tel:+4168857330" style={{ color: "wheat", alignSelf: "end" }}>
                             <span className="footer_contact_us_links">
                                 416-885-7330
                             </span>
                         </a>
                     </span>
                     <span className='footer_contact_us_info_row'>Email:
-                        <a href="mailto:info@cabuildingsupply.ca" style={{ color: 'wheat' }}>
+                        <a href="mailto:info@cabuildingsupply.ca" style={{ color: 'wheat', alignSelf: "end" }}>
                             <span className="footer_contact_us_links">
                                 info@cabuildingsupply.ca
                             </span>
